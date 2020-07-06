bf0aa638e808e7db9725ad674fb613fa6c97bbc4
Kevin Duell <kdubzot9@gmail.com>
2020-07-06 08:53:37 -0700

    Capture progress with styles object, getting away from inline styles.

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, getEnteredGoal] = useState('');
+
+  const goalInputHandler = (enteredText)  => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {};
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen} >
+    <View style={styles.inputContainer}>
+      <TextInput placeholder="Course Goal"
+      style={styles.textInput}
+      onChangeText={goalInputHandler}
+      value={enteredGoal}
+      />
+      <Button title="Add" />
+    </View>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {padding:50},
+  inputContainer: {flexDirection: 'row', justifyContent:'space-between', alignItems:'center'},
+  textInput:{ width: "80%", borderColor:'black', borderWidth: 1, padding:10}
+
 });
