10f4abca34e739974c307d418123bf2695488820
Kevin Duell <kdubzot9@gmail.com>
2020-07-16 13:13:47 -0700

    Capture working scrolling view checkpoint before FlatList implementation.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -24,20 +24,21 @@ export default function App() {
       />
       <Button title="Add" onPress={addGoalHandler}/>
     </View>
-    <View>{courseGoals.map((goal) => <View style={styles.listItem}><Text key={goal}>{goal}</Text></View>)}</View>
+    <ScrollView>{courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}</ScrollView>
     </View>
   );
 
 }
 
 const styles = StyleSheet.create({
-  screen: {padding:35},
+  screen: {padding:50},
   inputContainer: {flexDirection: 'row', justifyContent:'space-between', alignItems:'center'},
   textInput:{ width: "80%", borderColor:'black', borderWidth: 1, padding:10},
   listItem: {padding:10,
-            backgroundColor: '#ccc',
-            borderColor: 'black',
-            borderWidth: 1
-          }
+             marginVertical: 10,
+             backgroundColor: '#ccc',
+             borderColor: 'black',
+             borderWidth: 1
+            }
 
 });
