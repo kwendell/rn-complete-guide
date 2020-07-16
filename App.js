b46b82610319912a5c6efbd1d346d59f1707c561
Kevin Duell <kdubzot9@gmail.com>
2020-07-16 13:56:43 -0700

    Capture App.js and GoalItem.js separated out as a customer component.

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
+import GoalItem from './components/GoalItem';
+
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
@@ -25,8 +27,8 @@ export default function App() {
       <Button title="Add" onPress={addGoalHandler}/>
     </View>
     <FlatList data={courseGoals} 
-               renderItem={ itemData => (<View  style={styles.listItem}><Text>{itemData.item.value}</Text></View>) }/>
-    
+               renderItem={ itemData => <GoalItem title={itemData.item.value}/>}  
+    />
     </View>
   );
 
@@ -36,11 +38,5 @@ const styles = StyleSheet.create({
   screen: {padding:50},
   inputContainer: {flexDirection: 'row', justifyContent:'space-between', alignItems:'center'},
   textInput:{ width: "80%", borderColor:'black', borderWidth: 1, padding:10},
-  listItem: {padding:10,
-             marginVertical: 10,
-             backgroundColor: '#ccc',
-             borderColor: 'black',
-             borderWidth: 1
-            }
-
+  
 });
